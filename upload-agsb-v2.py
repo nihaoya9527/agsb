13f80c737aa145a2ddd4a2e0d13e572302f7d824
nihaoya9527 <gxnas-ds918@outlook.com>
2025-06-26 00:53:41 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "nihaoya9527"         # 用户名
+UUID = "81a04e27-544e-4a7a-87f6-c5eb60cb66c2"                     # UUID，留空则自动生成
+PORT = 49523                   # Vmess端口，留空或0则自动生成
+DOMAIN = "agsb.nihao.ggff.net"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiOTRlNGEyMWVhMmIyODY4NTc3YjE3Yjg3ZDkwNjFmZGQiLCJ0IjoiMWUxYTdiMDAtYTRhOC00YmJiLTg5N2UtODEwMGQ0YTIyYzhiIiwicyI6Ik1XSTFNV1JqTURRdE9HVmpNQzAwWVRoa0xUazJOell0TTJFelpHSTJNR1psTkRFMiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
